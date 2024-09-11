e104d7e82a48914fc03bde2c704beed7dcfc462d
AngelAlx04 <Angelrap.2222@gmail.com>
2024-09-11 14:34:49 -0500

    First activity of React

---
+++ src/FirstApp.js
@@ -0,0 +1,12 @@
+
+function FirstApp(){
+  return(
+    <>
+    <h1 style={{color:'darkred', textAlign: 'center'}}>Estoy Aprendiendo React</h1>
+    <span style={{fontSize: '20px', textAlign: 'center'}}>10</span>
+
+    </>
+  )
+};
+
+export default FirstApp;
