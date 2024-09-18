e1bdfdf5eee02245bf579af52f9542c5b7a8a77f
AngelAlx04 <Angelrap.2222@gmail.com>
2024-09-18 15:49:29 -0500

    Upload Custom Hooks, counter

--- src/FirstApp.js
+++ src/FirstApp.js
@@ -1,28 +1,18 @@
-import { useState } from "react";
+import React from 'react';
+import { useCounter } from './Hooks/UseCounter';
 
 const FirstApp = ({ value }) => {
-    const [counter, setCounter] = useState(value);
 
-    const handleAdd = () => {
-        setCounter(counter + 1);
-    };
-
-    const handleSubstract = () => {
-        setCounter(counter - 1);
-    };
-
-    const handleReset = () => {
-        setCounter(value);
-    };
+    const { counter, handleAdd, handleSubstract, handleReset } = useCounter(value);
 
     return (
         <>
-            <h1>Counter</h1>
+            <h1>Contador</h1>
             <span>{ counter }</span>
             <br />
             <button onClick={handleAdd}>+1</button>
             <button onClick={handleSubstract}>-1</button>
-            <button onClick={handleReset}>Reset</button>
+            <button onClick={handleReset}>Resetear</button>
         </>
     );
 };
