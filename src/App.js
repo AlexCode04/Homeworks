8db4a7a25ca2df30868689db979edcb41bbd18c7
AngelAlx04 <Angelrap.2222@gmail.com>
2024-09-25 16:15:29 -0500

    Challenge memorize

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Father } from './Components/Father';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Father />       
+      </header>
+    </div>
+  );
+}
+
+export default App;
