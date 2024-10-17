084194db4c3fd23f96b44fd6bea13de0886d31e4
AngelAlx04 <Angelrap.2222@gmail.com>
2024-10-17 10:42:40 -0500

    Fake login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { useEffect, useReducer } from "react";
-import NavComponent from "./components/NavComponent";
 import { Route, Routes } from "react-router-dom";
-import Experience from "./pages/Experience";
-import Portfolio from "./pages/Portfolio";
-import AboutMe from "./pages/AboutMe";
-import Home from "./pages/Home";
+import { UserProvider } from "./components/UserProvider";
+import Login from "./components/Login";
+import { PrivateRoute } from "./components/PrivateRoute";
+import HomePage from "./components/HomePage";
+
 function App() {
   
   return (
     <>
-      <NavComponent></NavComponent>
-      <Routes >
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/experience" element={<Experience/>}></Route>
-        <Route path="/portfolio" element={<Portfolio/>}></Route>
-        <Route path="/about-me" element={<AboutMe/>}></Route>
-      </Routes>
+      <UserProvider>
+        <Routes>
+          <Route path="/login" element={<Login/>} />
+          <Route path="/" element={<PrivateRoute><HomePage /></PrivateRoute>} />
+        </Routes>
+      </UserProvider>
     </>
   );
 }
